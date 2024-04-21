e4e60aa1a43e4eb128694b22acf2ce96e0777862
lost-rob0t <lost-rob0t@users.noreply.github.com>
2024-04-21 01:35:22 +0000

    Update packages

--- packages/default.nix
+++ packages/default.nix
@@ -382,6 +382,7 @@
   "diffoutput" = ./diffoutput.json;
   "digitalocean" = ./digitalocean.json;
   "dik" = ./dik.json;
+  "dim" = ./dim.json;
   "dimage" = ./dimage.json;
   "dimscmd" = ./dimscmd.json;
   "dimscord" = ./dimscord.json;
